5c41592b508437b068832808099b14a8fe64bac0
Candela94 <candelafsg@alumnos.cei.es>
2025-01-08 15:52:33 +0100

    about y mattura

---
+++ js/mattura/mattura.js
@@ -0,0 +1,49 @@
+
+
+// --------------------------
+//      Variables
+//---------------------------
+
+const headers = document.querySelectorAll(".Acordeon-header");
+const contenido = document.querySelectorAll(".Acordeon-contenido");
+
+
+
+// --------------------------
+//   Funciones y listeners
+//---------------------------
+
+headers.forEach((header) => {
+
+    header.addEventListener("click", ( )=> {
+
+        //Buscamos el siguiente hermano directo relacionado 
+        const cont = header.nextElementSibling;
+
+        if(cont.classList.contains("active")) {
+            cont.classList.remove("active");
+
+        } else {
+            document.querySelectorAll(".Acordeon-contenido").forEach((content) => {
+                content.classList.remove("active");
+
+            });
+
+            cont.classList.add("active");
+        }
+
+
+
+
+
+        // contenido.forEach((content) => {
+        //     content.classList.remove("active");
+
+        // });
+
+        // cont.classList.toggle("active");
+
+
+    });
+
+});
